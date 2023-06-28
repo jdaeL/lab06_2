fdac7ace5b68c37eecff47560a90b6745bfdc03a
jdaeL <joedaniel0804@gmail.com>
2023-06-28 00:47:54 -0500

    settings

--- src/mysite/mysite/settings.py
+++ src/mysite/mysite/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
@@ -127,7 +127,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','build')
 
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/uploads/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
