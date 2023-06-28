43fd6c2535dc99c99b00f972a2921ced0630bb26
jdaeL <joedaniel0804@gmail.com>
2023-06-27 20:15:25 -0500

    proyecyo subido

---
+++ src/mysite/travello/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Destination (models.Model):
+    nombreCiudad = models.CharField(max_length=100)
+    imagenCiudad = models.ImageField(upload_to='pics')
+    descripcionCiudad = models.TextField()
+    precioTour = models.IntegerField()
+    ofertaTour = models.BooleanField(default=False)
